4514180614a93a2478c869d4f1041e3da8e093fd
м <174154109+n11kol11c@users.noreply.github.com>
2025-12-09 20:47:58 +0100

    Update bootloader.asm

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,13 +1,55 @@
-; simple bootloader with hlt looping
-; booting from floppy disk (img)
+; simple bootloader written in asm (nasm)
+; currently booting from floppy disk
+
 org 0x7C00
 bits 16
 
+; make a newline nasm macro
+%define ENDL 0x0D, 0x0A
+
+; automaticly jumps to main because of 
+; functions infront the main program register
+start:
+    jmp main
+
+; push sectors for printing to screen
+puts:
+    push si
+    push ax
+
+.loop:
+    lodsb
+    or al, al
+    jz .done
+
+    mov ah, 0x0e
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
 main:
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    mov ss, ax
+    mov sp, 0x7C00
+
+    mov si, msg_hello
+    call puts
+
     hlt
 
 .halt:
     jmp .halt
+
+
+msg_hello: db "Welcome to WhaleOS!", ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
