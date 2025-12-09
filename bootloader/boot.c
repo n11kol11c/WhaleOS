01a1a05161b3f218ed877de22c3e30b67f128e66
м <174154109+n11kol11c@users.noreply.github.com>
2025-12-09 23:03:12 +0100

    Update boot.c

--- bootloader/boot.c
+++ bootloader/boot.c
@@ -9,13 +9,13 @@ void logf(const char *string, ...);
 void err(const char *string, unsigned int code);
 void boot_init(void);
 
-void logf(const char *fmt, ...) {
+void logf(const char *string, ...) {
     char buffer[MAX_BUF];
 
     va_list args;
-    va_start(args, fmt);
-    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_start(args, string);
+    vsnprintf(buffer, sizeof(buffer), string, args);
     va_end(args);
 
-    printf("%s", buffer);
+    printf("%s\n", buffer);
 }
