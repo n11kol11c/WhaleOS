f7c5e5466129f3290756a735abaec2ab8ebdfa32
м <174154109+n11kol11c@users.noreply.github.com>
2025-12-09 23:02:49 +0100

    Update bootloader.asm

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -7,27 +7,27 @@ bits 16
 jmp short start
 nop
 
-bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
+bdb_oem:                    db 'MSWIN4.1'           
 bdb_bytes_per_sector:       dw 512
 bdb_sectors_per_cluster:    db 1
 bdb_reserved_sectors:       dw 1
 bdb_fat_count:              db 2
 bdb_dir_entries_count:      dw 0E0h
-bdb_total_sectors:          dw 2880                 ; 2880 * 512 = 1.44MB
-bdb_media_descriptor_type:  db 0F0h                 ; F0 = 3.5" floppy disk
-bdb_sectors_per_fat:        dw 9                    ; 9 sectors/fat
+bdb_total_sectors:          dw 2880                 
+bdb_media_descriptor_type:  db 0F0h                
+bdb_sectors_per_fat:        dw 9                  
 bdb_sectors_per_track:      dw 18
 bdb_heads:                  dw 2
 bdb_hidden_sectors:         dd 0
 bdb_large_sector_count:     dd 0
 
 ; extended boot record
-ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, useless
-                            db 0                    ; reserved
+ebr_drive_number:           db 0                    
+                            db 0                    
 ebr_signature:              db 29h
-ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number, value doesn't matter
-ebr_volume_label:           db 'NANOBYTE OS'        ; 11 bytes, padded with spaces
-ebr_system_id:              db 'FAT12   '           ; 8 bytes
+ebr_volume_id:              db 12h, 34h, 56h, 78h   
+ebr_volume_label:           db 'NANOBYTE OS'        
+ebr_system_id:              db 'FAT12   '           
 
 start:
     jmp main
@@ -40,12 +40,12 @@ puts:
     push bx
 
 .loop:
-    lodsb               ; loads next character in al
-    or al, al           ; verify if next character is null?
+    lodsb               
+    or al, al           
     jz .done
 
-    mov ah, 0x0E        ; call bios interrupt
-    mov bh, 0           ; set page number to 0
+    mov ah, 0x0E        
+    mov bh, 0           
     int 0x10
 
     jmp .loop
